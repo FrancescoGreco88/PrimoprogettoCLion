d8e257578e3350c6be0556f9232a554bd01d369f
Francesco_Greco <grecofrancesco524@gmail.com>
2020-02-27 12:12:12 +0100

    fix_primoprogetto

---
+++ inserimento_somma_due_numeri.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int somma;
+    int num1;
+    int num2;
+
+    printf("Inserisci il primo valore: ");
+    scanf("%d", &num1);
+
+    printf("Inserisci il secondo valore: ");
+    scanf("%d", &num2);
+
+    somma = num1 + num2;
+
+    printf("La somma dei due numeri e' la seguente: %d", somma);
+    return 0;
+}
